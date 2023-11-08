8bb69138320b4ebbce8c0525407dc67ba06b7256
Abhinav Gorantla <abhinav20016@gmail.com>
2023-11-07 23:55:12 -0700

    Added code to compute intrinsic dimensionality of labels

--- phase_3_task_0.py
+++ phase_3_task_0.py
@@ -11,20 +11,8 @@ from pymongo.server_api import ServerApi
 from tqdm import tqdm
 import sys
 
-def find_lowest_index_greater_than(arr, target_value):
-    left, right = 0, len(arr) - 1
-    lowest_index = None
-
-    while left <= right:
-        mid = left + (right - left) // 2
-
-        if arr[mid] > target_value:
-            lowest_index = mid  # Update the lowest index found so far
-            right = mid - 1
-        else:
-            left = mid + 1
-
-    return lowest_index
+from helper_functions import find_lowest_index_greater_than
+from database_connection import connect_to_mongo
 
 transforms = transforms.Compose([
             transforms.ToTensor(),
@@ -33,30 +21,32 @@ transforms = transforms.Compose([
 uri = "mongodb://localhost:27017/cse515_project_phase1"
 
 # Create a new client and connect to the server
-mongo_client = MongoClient(uri, server_api=ServerApi('1'))
+mongo_client = connect_to_mongo()
 
-# Send a ping to confirm a successful connection
-try:
-    mongo_client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-dbnme = mongo_client.cse515_project_phase1
-collection = dbnme.phase2_features
+dbname = mongo_client.cse515_project_phase1
+collection = dbname.phase2_features
+rep_image_collection = dbname.phase2_representative_images
 
 image_data = []
 
-# Simulate iterating through a database query result
-image_features = collection.find()
-document_count = collection.count_documents({})
+task_selection = int(input("What is your desired output?\n1. Intrinsic dimensionality of even numbered Caltech101 images.\n2. Intrinsic dimensionality of labels of even numbered images in Caltech101\n"))
 
-print("Database query complete")
-for image in tqdm(image_features, desc="Loading Progress", unit="images", total=document_count):
-    # print(image["image_id"])
-    image_data.append(np.array(image[str(sys.argv[1])]).flatten())
+match task_selection:
+    case 1:
+        image_features = collection.find()
+        document_count = collection.count_documents({})
 
-# Convert the list of flattened images to a numpy array
+        print("Database query complete")
+        for image in tqdm(image_features, desc="Loading Progress", unit="images", total=document_count):
+            image_data.append(np.array(image[str(sys.argv[1])]).flatten())
+    case 2:
+        image_features = rep_image_collection.find({"feature": str(sys.argv[1])})
+        document_count = rep_image_collection.count_documents({"feature": str(sys.argv[1])})
+
+        print("Database query complete")
+        for image in tqdm(image_features, desc="Loading Progress", unit="images", total=document_count):
+            image_data.append(np.array(image["feature_value"]).flatten())
+
 image_data = np.array(image_data)
 
 n_components = len(image_data[0])
