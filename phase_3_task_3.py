47e0c0e6224df8b9e4e38ca875c383feb76af876
sammy-16 <rohansamueljustin@gmail.com>
2023-11-17 15:41:36 -0700

    Personalized Page Rank Untested

--- phase_3_task_3.py
+++ phase_3_task_3.py
@@ -1,26 +1,111 @@
+import torch
+import cv2
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
 from tqdm import tqdm
 from database_connection import connect_to_mongo
-import networkx as nx 
+import networkx as nx
+import torchvision.transforms as transforms
+from torchvision import datasets
+from color_moment import extract_color_moment
+from hog import extract_hog
+from resnet import extract_from_resnet
 
-# NOT FINISHED
-def personalized_page_rank_predict(G, odd_data, even_data, k):
+transforms = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+dataset = datasets.Caltech101(
+    'your_path', transform=transforms, download=True)
+data_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=4, shuffle=True, num_workers=8)
+
+def load_odd_image_data(feature):
+    odd_number_images = []
+    for image_ID in range(8677):
+        if image_ID % 2 != 0:
+            img, label = dataset[image_ID]
+
+            resized_img = [cv2.resize(i, (300, 100)) for i in img.numpy()]
+            resized_resnet_img = [cv2.resize(i, (224, 224)) for i in img.numpy()]
+
+            # checking if the image has 3 channels
+            if len(resized_img) == 3:
+                if feature == "color_moment":
+                    descriptor = extract_color_moment(resized_img)
+                elif feature == "hog":
+                    descriptor = extract_hog(resized_img)
+                elif feature == "avgpool":
+                    descriptor_temp = extract_from_resnet(resized_resnet_img)
+                    descriptor = descriptor_temp["avgpool"]
+                elif feature == "layer3":
+                    descriptor_temp = extract_from_resnet(resized_resnet_img)
+                    descriptor = descriptor_temp["layer3"]
+                elif feature == "fc":
+                    descriptor_temp = extract_from_resnet(resized_resnet_img)
+                    descriptor = descriptor_temp["fc"]
+                entry = {
+                    "image_id": str(image_ID),
+                    "label": label,
+                    "feature": descriptor
+                }
+                odd_number_images.append(entry)
+    return odd_number_images
+
+def fetch_data_from_db(descriptor):
+    data = []
+    for image_data in collection.find():
+        if descriptor in image_data:
+            entry = {
+                'image_id': image_data['image_id'],
+                'feature': np.array(image_data[descriptor]),
+                'label': image_data['target']
+            }
+            data.append(entry)
+    return data
+
+'''
+The below function creates a similarity graph which is computationally very intensive as it is adding every node and edge separately in a loop
+It could potentially dilute the importance of the most relevant similarities as it is a fully connected graph for 4000< images.
+'''
+# def create_similarity_graph(data):
+#     G = nx.Graph()
+#     for i, entry in enumerate(data):
+#         G.add_node(i, image_id=entry['image_id'], label=entry['label'])
+#         for j, other_entry in enumerate(data):
+#             if i == j:
+#                 continue
+#             G.add_node(j, image_id=other_entry['image_id'], label=other_entry['label'])
+#             G.add_edge(i, j, weight=euclidean_distance(entry['feature'], other_entry['feature']))
+#     return G
+
+def create_similarity_graph(data, n):
+    G = nx.Graph()
+    for i, entry in enumerate(data):
+        G.add_node(entry['image_id'], label=entry['label'])
+        
+        # Calculate similarities with all other nodes
+        similarities = [(other_entry['image_id'], 1 / (1 + euclidean_distance(entry['feature'], other_entry['feature']))) for other_entry in data if other_entry['image_id'] != entry['image_id']]
+        
+        # Sort by similarity and take top n
+        top_n_similarities = sorted(similarities, key=lambda x: x[1], reverse=True)[:n]
+
+        for other_image_id, similarity in top_n_similarities:
+            G.add_edge(entry['image_id'], other_image_id, weight=similarity)
+
+    return G
+
+def personalized_page_rank_predict(graph, test_data, alpha):
     predictions = {}
-    for test_entry in odd_data:
+    for test_entry in test_data:
         # Create personalization vector
-        personalization = {entry['image_id']: 1 if entry['image_id'] == test_entry['image_id'] else 0 for entry in even_data}
-        ranks = nx.pagerank(G, personalization=personalization)
-
-        # Get top k nodes and their labels
-        top_k_nodes = sorted(ranks, key=ranks.get, reverse=True)[:k]
-        top_k_labels = [even_data[node]['label'] for node in top_k_nodes if node in even_data]
-
-        # Predict label
-        predicted_label = max(set(top_k_labels), key=top_k_labels.count)
-        predictions[test_entry['image_id']] = predicted_label
-
+        personalization = {node: 1 if graph.nodes[node]['label'] == test_entry['label'] else 0 for node in graph.nodes}
+        
+        ranks = nx.pagerank(graph, personalization=personalization, alpha=alpha)
+        
+        predicted_label = max(set(ranks), key=ranks.get)
+        predictions[test_entry['image_id']] = graph.nodes[predicted_label]['label']
     return predictions
 
 def euclidean_distance(x1, x2):
@@ -70,17 +155,25 @@ X_train, X_test = X_data[:split_index], X_data[split_index:]
 y_train, y_test = y_data[:split_index], y_data[split_index:]
 
 classifier_choice = input("Select classifier (mnn, dt, ppr): ")
+feature_names = ["color_moment", "hog", "layer3", "avgpool", "fc"]
+print("1. Color Moment\n2. HoG\n3. Layer 3\n4. AvgPool\n5. FC\n6. Label-Label Similarity Matrix\n7. Image-Image Similarity Matrix")
+feature = int(input("Select one feature from above: "))
 
 if classifier_choice == 'mnn':
-    k = int(input("Enter k for m-NN: "))
-    y_pred = predict(X_test, X_train, y_train, k)
+    m = int(input("Enter m for m-NN: "))
+    y_pred = predict(X_test, X_train, y_train, m)
 elif classifier_choice == 'dt':
     dt_classifier = DecisionTreeClassifier()
     dt_classifier.fit(X_train, y_train)
     y_pred = dt_classifier.predict(X_test)
 elif classifier_choice == 'ppr':
-    k = int(input("Enter k for PPR: "))
-    # Create graph G and use personalized_page_rank_predict() function
+    jump_prob = int(input("Enter PPR random jump probability: "))
+    data = fetch_data_from_db(feature)
+    graph = create_similarity_graph(data, 10)
+    odd_number_images = load_odd_image_data()
+    predictions = personalized_page_rank_predict(graph, odd_number_images, jump_prob)
+    y_test = [entry['label'] for entry in odd_number_images]
+    y_pred = list(predictions.values())
 else:
     raise ValueError("Invalid classifier choice")
 
