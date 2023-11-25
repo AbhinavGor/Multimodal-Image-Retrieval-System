5c0ea2cf02ca18544f06186203de0b74f07ea56d
Abhinav Gorantla <abhinav20016@gmail.com>
2023-11-25 13:49:11 -0700

    Moved SVM class to a separate file

--- phase_3_task_5.py
+++ phase_3_task_5.py
@@ -4,45 +4,7 @@ import cvxpy as cp
 from sklearn.metrics import accuracy_score
 import ast
 
-# Create a SVM classifier
-class MultiClassSVM:
-    def __init__(self, C=1):
-        self.C = C
-        self.models = []
-
-    def fit(self, X, y):
-        classes = np.unique(y)
-        num_classes = len(classes)
-        
-        num_samples, num_features = X.shape
-
-        for i in range(num_classes):
-            # Create a binary classification problem for each class
-            binary_labels = np.where(y == classes[i], 1, -1)
-
-            # Define the variables
-            w = cp.Variable(num_features)
-            b = cp.Variable()
-
-            # Define the objective function
-            obj = cp.Minimize(0.5 * cp.norm(w, 2) + self.C * cp.sum(cp.pos(1 - cp.multiply(binary_labels, (X @ w + b)))))
-
-            # Define the problem
-            prob = cp.Problem(obj)
-
-            # Solve the problem
-            prob.solve()
-
-            # Save the model parameters
-            self.models.append((w.value, b.value))
-
-    def predict(self, X):
-        scores = []
-        for w, b in self.models:
-            scores.append(X @ w + b)
-            # print(w, b)
-        scores = np.array(scores).T
-        return [np.argmax(scores, axis=1)]
+from MultiClassSVM import MultiClassSVM
 
 # Load the data from the CSV file
 df = pd.read_csv('task_4_output.csv')
@@ -69,7 +31,7 @@ IDs = IDs[test_mask]
 
 print(X_test.shape)
 # Create a SVM classifier
-clf = MultiClassSVM(C=4)
+clf = MultiClassSVM(C=2000)
 
 # Train the classifier
 clf.fit(X_train, y_train)
